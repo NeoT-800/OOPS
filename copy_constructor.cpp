aa773908f97efa30b2ec149bf8e728d1bfe061d7
NeoT-800 <116675389+NeoT-800@users.noreply.github.com>
2023-03-18 00:46:29 +0530

    Add files via upload

---
+++ copy_constructor.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Student{
+    
+    private:
+        int age;
+
+    public: 
+        char *name;
+
+    Student(int age, char *name){ // Parameterized Constructor
+        this -> age = age;
+
+        //Shallow Copy
+        // this -> name = name;
+
+        // Deep Copy
+        this -> name = new char[strlen(name)+1]; // Created new memory
+        strcpy(this -> name, name);
+
+        // Note: Assignment operator and the default copy constructor makes a shallow copy.
+
+    }
+
+    // Copy Constructor
+    Student(Student const &s){
+        this -> age = s.age;  // Shallow copy
+        this -> name = new char[strlen(s.name)+1];  // Deep copy
+        strcpy(this -> name, s.name);
+    }
+
+    void display(){
+        cout<<age<<" "<<name<<endl;
+    }
+};
+
+int main(){
+    char name[] = "abcd";
+    Student s1(20,name);
+    s1.display();
+
+    Student s2(s1);
+    s2.name[0] = 'x';
+    s1.display();
+    s2.display();
+
+}
